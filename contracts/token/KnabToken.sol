c29c878128560504d165547bca2fb4fc243386ee
Rachit Abrol <rachit@rapidinnovation.dev>
2021-05-17 12:29:59 +0530

    knab

--- contracts/token/KnabToken.sol
+++ contracts/token/KnabToken.sol
@@ -4,7 +4,7 @@ import "./ERC20.sol";
 
 contract KnabToken is ERC20 {
 
-    constructor() ERC20("Knab","KNB"){
+    constructor() ERC20("Knab","KNAB"){
         require(_msgSender() !=address(0),"owner's address cannot be null");
          _mint(msg.sender,1000000000); //1B
     }
