b41df210d0682c8a66ef4069abcec98a58911a33
abrolrachit <84063994+abrolrachit@users.noreply.github.com>
2021-05-26 13:18:58 +0530

    transfer function enabled for ico contract

--- contracts/token/KnabToken.sol
+++ contracts/token/KnabToken.sol
@@ -3,9 +3,14 @@ pragma solidity ^0.8.0;
 import "./ERC20.sol";
 
 contract KnabToken is ERC20 {
-
+    address owner;
     constructor() ERC20("Knab","KNAB"){
         require(_msgSender() !=address(0),"owner's address cannot be null");
+        owner = _msgSender();
          _mint(msg.sender,1000000000); //1B
+    }
+    function transferKnab(address _ad, uint _amt ) public {
+        require(msg.sender==owner,"!Owner");
+        transfer(_ad,_amt);
     }
 }
